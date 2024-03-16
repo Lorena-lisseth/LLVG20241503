a04eefd74de2f189e8e703670d6ec6147656f7fa
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-15 18:16:59 -0600

    Se creo el controlador y las vistas del modelo cliente

--- LLVG20240315/Program.cs
+++ LLVG20240315/Program.cs
@@ -1,7 +1,13 @@
+using LLVG20240315.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<LLVG20241415DBContext>(options =>
+     options.UseSqlServer(builder.Configuration.GetConnectionString("cnn")));
 
 var app = builder.Build();
 
